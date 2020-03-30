82ae5853a600ae1c4ff374f3dfaa915c14454d83
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2020-03-30 14:00:09 +0200

    T CVC 1

--- TestViaplayTests/TestViaplayTests.swift
+++ TestViaplayTests/TestViaplayTests.swift
@@ -23,12 +23,18 @@ class TestViaplayTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSectionCVCHeaderVM() {
+        let sectionsResponse1 = SectionsResponse(title: "Nice Title", description: "This is a very, very nice title", links: nil)
+        let sectionCVCHeaderVM1 = SectionCVCHeaderVM(sectionsResponse: sectionsResponse1)
+        
+        let sectionsResponse2 = SectionsResponse(title: nil, description: nil, links: nil)
+        let sectionCVCHeaderVM2 = SectionCVCHeaderVM(sectionsResponse: sectionsResponse2)
+        
+        XCTAssert(sectionCVCHeaderVM1.title == "Nice Title")
+        XCTAssert(sectionCVCHeaderVM1.description == "Description: This is a very, very nice title", "Not sdsdfdsfdsfsdf")
+        
+        XCTAssert(sectionCVCHeaderVM2.title == "No title")
+        XCTAssert(sectionCVCHeaderVM2.description == "Description: No description")
     }
-
 }
